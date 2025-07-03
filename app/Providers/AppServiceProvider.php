4de070c7048395e41ab6524e231dfb1b3e45a62a
Panji Mohammad <panji.muhammad92@gmail.com>
2025-07-03 16:38:10 +0700

    update layouts admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,9 +27,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        
-        /*Parameter pertama berisi tujuan dimana data dari View Composer akan di-passing, 
-        kita masukkan ecommerce.* yang berarti semua file yang berada didalam folder resources/views/ecommerce. 
+
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
+
+        /*Parameter pertama berisi tujuan dimana data dari View Composer akan di-passing,
+        kita masukkan ecommerce.* yang berarti semua file yang berada didalam folder resources/views/ecommerce.
         Parameter kedua adalah sumber datanya, maka dalam hal ini adalah CategoryComposer*/
         View::composer('ecommerce.*', 'App\Http\View\CategoryComposer');
         View::composer('ecommerce.*', 'App\Http\View\OrderComposer');
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="description" content="Barokah Jaya - Aplikasi Ecommerce">
   <meta name="author" content="Barokah Jaya">
   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
